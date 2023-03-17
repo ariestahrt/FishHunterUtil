d361b0bd2adc6ead6b648c2d77487c006d3e866b
Ariesta <ikadekagusariestaputra@gmail.com>
2023-03-17 18:03:48 +0800

    Ignore error when reading file

--- FishHunterUtil/features_extractor.py
+++ FishHunterUtil/features_extractor.py
@@ -5,7 +5,7 @@ from FishHunterUtil.html_util import *
 def get_dataset_features(dataset_path):
     # clean dataset
     # cleaning HTML
-    html_text = open(dataset_path+"/index.html", "r").read()
+    html_text = open(dataset_path+"/index.html", "r", errors='ignore').read()
     html_clean = remove_hidden_element(html_text=html_text, html_root=dataset_path+"/")
     
     # Save clean html to clean.html
@@ -34,16 +34,16 @@ def get_dataset_features(dataset_path):
     head.append(link)
 
     # Save to clean.html
-    with open(dataset_path+"/clean.html", "w", encoding="utf-8") as f:
+    with open(dataset_path+"/clean.html", "w", encoding="utf-8", errors='ignore') as f:
         f.write(lxml.html.tostring(html, encoding="unicode"))
 
     css_propval = extract_css_propval_from_file(dataset_path+"/assets/__clean__.css", document_root=f"{dataset_path}/")
     # Save css_propval to css.json
-    with open(dataset_path+"/css.json", "w", encoding="utf-8") as f:
+    with open(dataset_path+"/css.json", "w", encoding="utf-8", errors='ignore') as f:
         json.dump(css_propval, f, indent=4, sort_keys=True)
 
     # GET TEXT FEATURES
-    html_text = open(dataset_path+"/clean.html", "r").read()
+    html_text = open(dataset_path+"/clean.html", "r", errors='ignore').read()
     feature_text = get_rendered_text(html_text)
 
     # GET HTML STRUCTURE FEATURES
