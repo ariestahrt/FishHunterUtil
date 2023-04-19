aab1cc814012d6d8b4c9d08fafdc889fbd286dd6
Ariesta <ikadekagusariestaputra@gmail.com>
2023-04-19 13:14:54 +0800

    Add signal timeout selenium ss

--- FishHunterUtil/selenium_ss.py
+++ FishHunterUtil/selenium_ss.py
@@ -7,61 +7,74 @@ from selenium.webdriver.common.by import By
 from PIL import Image
 from io import BytesIO
 from time import sleep
+import signal
+
+def worker(url, save_to="test_ss.png"):
+    options = Options()
+    options.add_argument('--headless=new')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    # disable javascript
+    # options.add_argument('--disable-javascript')
+    # disable GPU
+    options.add_argument('--disable-gpu')
+
+    # disable cors
+    options.add_argument('--disable-web-security')
+    options.add_argument('--allow-running-insecure-content')
+    options.add_argument('--allow-insecure-localhost')
+    options.add_argument('--allow-file-access-from-files')
+    options.add_argument('--allow-file-access')
+    options.add_argument('--allow-cross-origin-auth-prompt')
+
+    prefs = {}
+    # prefs["webkit.webprefs.javascript_enabled"] = False
+    # prefs["profile.content_settings.exceptions.javascript.*.setting"] = 2
+    # prefs["profile.default_content_setting_values.javascript"] = 2
+    # prefs["profile.managed_default_content_settings.javascript"] = 2
+
+    options.add_experimental_option("prefs", prefs)
+
+    driver = webdriver.Chrome(service=Service("/usr/local/bin/chromedriver"), options=options, desired_capabilities=DesiredCapabilities.CHROME)
+
+    driver.maximize_window()
+    driver.set_window_size(1920, 1080)
+    driver.get(url)
+
+    # wait for page to load
+    driver.implicitly_wait(2)
+
+    # stop page loading
+    driver.execute_script("window.stop();")
+    
+    ss = driver.get_screenshot_as_png()
+
+    # convert to jpg
+    img = Image.open(BytesIO(ss))
+    img = img.convert("RGB")
+    img.save(save_to)
+
+    driver.quit()
+
+class TimeoutException(Exception):
+    pass
+
+def timeout_handler(signum, frame):
+    raise TimeoutException("Function call timed out")
 
 def screenshot(url, save_to="test_ss.png"):
     while True:
         try:
-            options = Options()
-            options.add_argument('--headless=new')
-            options.add_argument('--no-sandbox')
-            options.add_argument('--disable-dev-shm-usage')
-            # disable javascript
-            # options.add_argument('--disable-javascript')
-            # disable GPU
-            options.add_argument('--disable-gpu')
-
-            # disable cors
-            options.add_argument('--disable-web-security')
-            options.add_argument('--allow-running-insecure-content')
-            options.add_argument('--allow-insecure-localhost')
-            options.add_argument('--allow-file-access-from-files')
-            options.add_argument('--allow-file-access')
-            options.add_argument('--allow-cross-origin-auth-prompt')
-
-            prefs = {}
-            # prefs["webkit.webprefs.javascript_enabled"] = False
-            # prefs["profile.content_settings.exceptions.javascript.*.setting"] = 2
-            # prefs["profile.default_content_setting_values.javascript"] = 2
-            # prefs["profile.managed_default_content_settings.javascript"] = 2
-
-            options.add_experimental_option("prefs", prefs)
-
-            driver = webdriver.Chrome(service=Service("/usr/local/bin/chromedriver"), options=options, desired_capabilities=DesiredCapabilities.CHROME)
-
-            driver.maximize_window()
-            driver.set_window_size(1920, 1080)
-            driver.get(url)
-
-            # wait for page to load
-            # driver.implicitly_wait(10)
-            # wait 5 seconds for page to load
-            sleep(5)
-
-            # stop page loading
-            driver.execute_script("window.stop();")
-            
-            ss = driver.get_screenshot_as_png()
-
-            # convert to jpg
-            img = Image.open(BytesIO(ss))
-            img = img.convert("RGB")
-            img.save(save_to)
-
-            driver.quit()
-            return
+            signal.signal(signal.SIGALRM, timeout_handler)
+            signal.alarm(5)
+            worker(url, save_to)
+            signal.alarm(0)
+            break
+        except TimeoutException as e:
+            print("TimeoutException: ", e)
+            continue
         except Exception as e:
-            print("Exception when taking screenshot")
-            print(e)
+            print("Exception: ", e)
             continue
 
 if __name__ == "__main__":
